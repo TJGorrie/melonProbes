4333bf1c2d383af31661446ed10202593ea01f33
Gizem <ga19526@essex.ac.uk>
2020-06-03 21:17:39 +0100

    TestProbes RGChannelSet

---
+++ R/testProbesRGChannelSet.R
@@ -0,0 +1,28 @@
+
+
+#TestProbes RGSet (RGChannelSet)
+
+setMethod(f= "testProbes",
+          signature(betas="RGChannelSet"),
+          definition=function(
+            betas,manifest = c('450k', 'EPIC'), beadcounts = NULL, detection = NULL, 
+            nb = .2, np = .2, nvar =.5, ot,
+            nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)
+        
+              { #betas of object
+                betRG <- getBeta(betas)
+                
+                #ot
+                ot <- got(betas)
+                
+                #beadcount ??
+                NBeads <- beadcount(betas)
+                #detectionP
+                dp <- detectionP(betas)
+                
+                testProbes(betas=betRG,manifest = c('450k', 'EPIC'), beadcounts = NBeads, detection = dp, 
+                           nb = .2, np = .2, nvar =.5, ot=ot,
+                           nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)  
+            
+          }
+              )
