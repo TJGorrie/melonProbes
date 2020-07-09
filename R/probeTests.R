ebe68eceba68adf8a3e6699adbf93c2cff8f0c9f
Leo Schalkwyk <lschal@essex.ac.uk>
2020-07-09 09:41:39 +0100

    comment

--- R/probeTests.R
+++ R/probeTests.R
@@ -86,7 +86,7 @@ testProbes <- function(betas, manifest = c('450k', 'EPIC'), beadcounts = NULL, d
 #'@describeIn TestProbe Method for RGChannelSet object (Minfi)
 #'@method setMethod( f="testProbes",signature(betas="RGChannelSet"), definition = function(betas,manifest = c('450k', 'EPIC'), beadcounts = NULL, detection = NULL, nb = .2, np = .2, nvar =.5, ot,nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)
 
-?? adopted from pfilter method
+#?? adopted from pfilter method
 #' @section Methods: \describe{ \item{list("signature(betas =
 #' \"MethyLumiSet\")")}{ This is used for performing the testProbes method on
 #' MethyLumiSet objects produced by methylumiR.  } \item{list("signature(betas =
