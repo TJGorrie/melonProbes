4c2e8bfc6e85fafe093cc7d53ed85acc2fdc5e71
Gizem <ga19526@essex.ac.uk>
2020-07-21 21:13:04 +0100

    Return to object with fData column

--- R/testProbesRGChannelSet.R
+++ R/testProbesRGChannelSet.R
@@ -20,9 +20,12 @@ setMethod(f= "testProbes",
                 #detectionP
                 dp <- detectionP(betas)
                 
-                testProbes(betas=betRG,manifest = c('450k', 'EPIC'), beadcounts = NBeads, detection = dp, 
+                out <- testProbes(betas=betRG,manifest = c('450k', 'EPIC'), beadcounts = NBeads, detection = dp, 
                            nb = .2, np = .2, nvar =.5, ot=ot,
                            nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)  
             
-          }
+                fData(betas) <- cbind(fData(betas),out)
+                return(betas)
+                
+                 }
               )
