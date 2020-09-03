17588dc2348ea4363b310df5a071d147cae9d85b
Gizem <ga19526@essex.ac.uk>
2020-09-03 17:38:55 +0100

    MethylumiSet and RGChannelSet

--- R/testProbesMethylumiSet.R
+++ R/testProbesMethylumiSet.R
@@ -4,7 +4,7 @@
 setMethod( f="testProbes",
           signature(betas="MethyLumiSet"),
           definition = function(betas,manifest = c('450k', 'EPIC'), beadcounts = NULL, detection = NULL, 
-                                nb = .2, np = .2, nvar =.5, ot,
+                                nb = .2, np = .2, nvar = .5, ot,
                                 nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)
             {
 #betas of MethylumiSet object            
@@ -27,8 +27,8 @@ setMethod( f="testProbes",
 #detection 
   pv <- betas@assayData$pval
   out <- testProbes(betas=bet,manifest = c('450k', 'EPIC'), beadcounts = bc, detection = pv, 
-             nb = .2, np = .2, nvar =.5, ot=ot,
-             nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)
+             nb = nb, np = np, nvar = nvar, ot=ot,
+             nbCount = nbCount, nbThresh = nbThresh, pvCount = pvCount, pvThresh = pvThresh, nvarThresh = nvarThresh)
 
 
 
--- R/testProbesRGChannelSet.R
+++ R/testProbesRGChannelSet.R
@@ -6,7 +6,7 @@ setMethod(f= "testProbes",
           signature(betas="RGChannelSet"),
           definition=function(
             betas,manifest = c('450k', 'EPIC'), beadcounts = NULL, detection = NULL, 
-            nb = .2, np = .2, nvar =.5, ot,
+            nb = .2, np = .2, nvar = .5, ot,
             nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)
         
               { betasP <- preprocessRaw(betas)
@@ -28,8 +28,8 @@ setMethod(f= "testProbes",
                 
                 
               out <-  testProbes(betas=betRG,manifest = c('450k', 'EPIC'), beadcounts = beadcount, detection = dp, 
-                           nb = .2, np = .2, nvar =.5, ot=ot,
-                           nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)  
+                           nb = nb, np = np, nvar = nvar, ot=ot,
+                           nbCount = nbCount, nbThresh = nbThresh, pvCount = pvCount, pvThresh = pvThresh, nvarThresh = nvarThresh)  
             
                 
 
