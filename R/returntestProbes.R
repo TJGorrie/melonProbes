6d9e3a780cf3f28786c8cf14cf6e8f0c5895a58b
Gizem <ga19526@essex.ac.uk>
2020-09-09 18:41:06 +0100

    Final changes

--- R/returntestProbes.R
+++ R/returntestProbes.R
@@ -45,13 +45,3 @@ setMethod( f="testProbes",
              
            })
 
-#MethylumiQC
-
-setMethod( f="testProbes",
-           signature(betas="MethylumiQC"),
-           definition = function(betas,manifest = c('450k', 'EPIC'), beadcounts = NULL, detection = NULL, 
-                                 nb = .2, np = .2, nvar =.5, ot,
-                                 nbCount = 3, nbThresh = 0.05, pvCount = 0.05, pvThresh = 0.01, nvarThresh = 0.05)
-           {message("We are sorry. For running testProbes function, using RGChannelSetExtended or MEthylumiSet object is strongly recommended.")
-             
-           })
